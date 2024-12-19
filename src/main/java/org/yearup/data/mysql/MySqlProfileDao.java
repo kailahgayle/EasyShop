95f497328051775005920807789006de2c23163b
kailahgayle <kailahkg@gmail.com>
2024-12-19 03:57:48 -0500

    updates in SqlProfileDao for profile controller

--- src/main/java/org/yearup/data/mysql/MySqlProfileDao.java
+++ src/main/java/org/yearup/data/mysql/MySqlProfileDao.java
@@ -1,47 +1,233 @@
 package org.yearup.data.mysql;
 
+
+
 import org.springframework.stereotype.Component;
+
 import org.yearup.models.Profile;
+
 import org.yearup.data.ProfileDao;
 
+
+
 import javax.sql.DataSource;
+
 import java.sql.*;
 
+
+
 @Component
+
 public class MySqlProfileDao extends MySqlDaoBase implements ProfileDao
+
 {
+
     public MySqlProfileDao(DataSource dataSource)
+
     {
+
         super(dataSource);
+
     }
 
+
+
     @Override
+
     public Profile create(Profile profile)
+
     {
+
         String sql = "INSERT INTO profiles (user_id, first_name, last_name, phone, email, address, city, state, zip) " +
+
                 " VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
-        try(Connection connection = getConnection())
+
+
+        try (Connection connection = getConnection())
+
         {
+
             PreparedStatement ps = connection.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
+
             ps.setInt(1, profile.getUserId());
+
             ps.setString(2, profile.getFirstName());
+
             ps.setString(3, profile.getLastName());
+
             ps.setString(4, profile.getPhone());
+
             ps.setString(5, profile.getEmail());
+
             ps.setString(6, profile.getAddress());
+
             ps.setString(7, profile.getCity());
+
             ps.setString(8, profile.getState());
+
             ps.setString(9, profile.getZip());
+
+
 
             ps.executeUpdate();
 
+
+
             return profile;
+
         }
+
         catch (SQLException e)
+
         {
+
             throw new RuntimeException(e);
+
         }
+
+    }
+
+
+
+    // Updates for user profile: Added method to retrieve a profile by user ID
+
+    @Override
+
+    public Profile getByUserId(int userId)
+
+    {
+
+        String sql = "SELECT * FROM profiles WHERE user_id = ?";
+
+
+
+        try (Connection connection = getConnection())
+
+        {
+
+            PreparedStatement ps = connection.prepareStatement(sql);
+
+            ps.setInt(1, userId);
+
+
+
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next())
+
+            {
+
+                return mapRow(rs);
+
+            }
+
+            else
+
+            {
+
+                return null;
+
+            }
+
+        }
+
+        catch (SQLException e)
+
+        {
+
+            throw new RuntimeException(e);
+
+        }
+
+    }
+
+
+
+    // Updates for user profile: Added the method to update a profile
+
+    @Override
+
+    public void update(Profile profile)
+
+    {
+
+        String sql = "UPDATE profiles SET first_name = ?, last_name = ?, phone = ?, email = ?, address = ?, city = ?, state = ?, zip = ? " +
+
+                "WHERE user_id = ?";
+
+
+
+        try (Connection connection = getConnection())
+
+        {
+
+            PreparedStatement ps = connection.prepareStatement(sql);
+
+            ps.setString(1, profile.getFirstName());
+
+            ps.setString(2, profile.getLastName());
+
+            ps.setString(3, profile.getPhone());
+
+            ps.setString(4, profile.getEmail());
+
+            ps.setString(5, profile.getAddress());
+
+            ps.setString(6, profile.getCity());
+
+            ps.setString(7, profile.getState());
+
+            ps.setString(8, profile.getZip());
+
+            ps.setInt(9, profile.getUserId());
+
+
+
+            ps.executeUpdate();
+
+        }
+
+        catch (SQLException e)
+
+        {
+
+            throw new RuntimeException(e);
+
+        }
+
+    }
+
+
+
+    // Addtnl method to map the result set to a Profiles object
+
+    private Profile mapRow(ResultSet rs) throws SQLException
+
+    {
+
+        Profile profile = new Profile();
+
+        profile.setUserId(rs.getInt("user_id"));
+
+        profile.setFirstName(rs.getString("first_name"));
+
+        profile.setLastName(rs.getString("last_name"));
+
+        profile.setPhone(rs.getString("phone"));
+
+        profile.setEmail(rs.getString("email"));
+
+        profile.setAddress(rs.getString("address"));
+
+        profile.setCity(rs.getString("city"));
+
+        profile.setState(rs.getString("state"));
+
+        profile.setZip(rs.getString("zip"));
+
+        return profile;
+
     }
 
 }
